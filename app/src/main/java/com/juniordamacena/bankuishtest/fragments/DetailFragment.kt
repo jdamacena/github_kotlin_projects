973e95f02e3b6acb3bcd0307ba407f132df124a5
Junior Damacena <juniordamacena980@gmail.com>
2022-08-05 20:24:58 -0300

    Implements a bottom sheet info card on the details screen and makes the Readme information the main object of that screen

--- app/src/main/java/com/juniordamacena/bankuishtest/fragments/DetailFragment.kt
+++ app/src/main/java/com/juniordamacena/bankuishtest/fragments/DetailFragment.kt
@@ -3,14 +3,14 @@ package com.juniordamacena.bankuishtest.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.card.MaterialCardView
 import com.juniordamacena.bankuishtest.R
 import com.juniordamacena.bankuishtest.databinding.FragmentDetailBinding
 import com.juniordamacena.bankuishtest.viewmodels.RepositoriesViewModel
@@ -19,6 +19,7 @@ import io.noties.markwon.image.glide.GlideImagesPlugin
 import io.noties.markwon.linkify.LinkifyPlugin
 import java.text.ParseException
 import java.text.SimpleDateFormat
+import kotlin.math.roundToInt
 
 class DetailFragment : Fragment() {
 
@@ -29,6 +30,8 @@ class DetailFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: RepositoriesViewModel by activityViewModels()
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<MaterialCardView>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,6 +44,16 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+
+        val peekDistance = resources.getDimension(R.dimen.bottom_sheet_peek_distance)
+
+        bottomSheetBehavior =
+            BottomSheetBehavior.from(binding.bottomSheet.standardBottomSheet).apply {
+                peekHeight = peekDistance.roundToInt()
+                isFitToContents = true
+                state = BottomSheetBehavior.STATE_COLLAPSED
+            }
 
         viewModel.readMeText.observe(viewLifecycleOwner) { readMeText ->
             // obtain an instance of Markwon
@@ -64,28 +77,26 @@ class DetailFragment : Fragment() {
                     subtitle = full_name
                 }
 
-                binding.lblDescription.text = description
+                binding.bottomSheet.lblDescription.text = description
 
                 try {
                     val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
                     val date = format.parse(updated_at)
-                    binding.lblUpdatedAt.text =
+                    binding.bottomSheet.lblUpdatedAt.text =
                         getString(R.string.last_updated_text, date?.toLocaleString() ?: updated_at)
                 } catch (e: ParseException) {
                     e.printStackTrace()
                 }
 
-                binding.lblHomepage.text = homepage
-                binding.lblHomepage.isVisible = !homepage.isNullOrBlank()
+                binding.bottomSheet.lblHomepage.text = homepage
+                binding.bottomSheet.lblHomepage.isVisible = !homepage.isNullOrBlank()
 
-                binding.lblNumStars.text = getString(R.string.num_stars_text, stargazers_count)
-                binding.lblNumWatchers.text = getString(R.string.num_watchers_text, watchers_count)
-                binding.lblNumForks.text = getString(R.string.num_forks_text, forks_count)
-
-                binding.btnOpenGithub.setOnClickListener {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(html_url))
-                    startActivity(intent)
-                }
+                binding.bottomSheet.lblNumStars.text =
+                    getString(R.string.num_stars_text, stargazers_count)
+                binding.bottomSheet.lblNumWatchers.text =
+                    getString(R.string.num_watchers_text, watchers_count)
+                binding.bottomSheet.lblNumForks.text =
+                    getString(R.string.num_forks_text, forks_count)
             }
         }
     }
@@ -96,6 +107,37 @@ class DetailFragment : Fragment() {
 
         (activity as AppCompatActivity).supportActionBar?.apply {
             subtitle = ""
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.details_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val repository = viewModel.selectedItem.value
+
+        return when (item.itemId) {
+            R.id.menu_info -> {
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                } else {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                }
+
+                return true
+            }
+            R.id.menu_open_github -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(repository?.html_url))
+                startActivity(intent)
+
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
